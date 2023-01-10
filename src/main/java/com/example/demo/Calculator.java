280ccff5ef3e5153e9670e8b8021d37f702fb0a7
miaumere <40804624+miaumere@users.noreply.github.com>
2023-01-10 19:23:28 +0100

    fixed view which wasn't showing

--- src/main/java/com/example/demo/Calculator.java
+++ src/main/java/com/example/demo/Calculator.java
@@ -1,21 +1,13 @@
 package com.example.demo;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Calculator extends Application {
-    private TextField displayField;
-    private double num1 = 0.0;
-    private double num2 = 0.0;
-    private String operator = "";
-    private boolean start = true;
-    private Stage primaryStage;
 
     public Calculator() {
     }
@@ -24,81 +16,12 @@ public class Calculator extends Application {
         launch(args);
     }
 
-    public void start(Stage primaryStage) {
-        this.primaryStage = primaryStage;
-        GridPane root = new GridPane();
-        root.setAlignment(Pos.CENTER);
-        root.setHgap(10.0);
-        root.setVgap(10.0);
-        root.setPadding(new Insets(25.0, 25.0, 25.0, 25.0));
-        this.displayField = new TextField();
-        this.displayField.setAlignment(Pos.CENTER_RIGHT);
-        root.add(this.displayField, 0, 0, 4, 1);
-        String[] buttons = new String[]{"7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "*", "C", "0", "=", "/"};
-
-        for(int i = 0; i < buttons.length; ++i) {
-            Button button = new Button(buttons[i]);
-            button.setMinSize(40.0, 40.0);
-            int row = i / 4;
-            int col = i % 4;
-            root.add(button, col, row + 1);
-            button.setOnAction((event) -> {
-                switch (((Button)event.getSource()).getText()) {
-                    case "C":
-                        this.start = true;
-                        this.displayField.setText("");
-                        this.operator = "";
-                        break;
-                    case "+":
-                    case "-":
-                    case "*":
-                    case "/":
-                        this.operator = buttonText;
-                        if (!this.start) {
-                            this.num1 = Double.parseDouble(this.displayField.getText());
-                            this.start = true;
-                        }
-                        break;
-                    case "=":
-                        if (!this.operator.isEmpty()) {
-                            this.num2 = Double.parseDouble(this.displayField.getText());
-                            double result = 0.0;
-                            switch (this.operator) {
-                                case "+":
-                                    result = this.num1 + this.num2;
-                                    break;
-                                case "-":
-                                    result = this.num1 - this.num2;
-                                    break;
-                                case "*":
-                                    result = this.num1 * this.num2;
-                                    break;
-                                case "/":
-                                    result = this.num1 / this.num2;
-                            }
-
-                            this.displayField.setText(String.valueOf(result));
-                            this.start = true;
-                        }
-                        break;
-                    default:
-                        if (this.start) {
-                            this.displayField.setText(buttonText);
-                            this.start = false;
-                        } else {
-                            TextField var10000 = this.displayField;
-                            String var10001 = this.displayField.getText();
-                            var10000.setText(var10001 + buttonText);
-                        }
-                }
-
-                Scene scene = new Scene(root, 250.0, 275.0);
-                primaryStage.setTitle("Calculator");
-                primaryStage.setScene(scene);
-                primaryStage.show();
-            });
-        }
-
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Calculator.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Calculator");
+        stage.setScene(scene);
+        stage.show();
     }
-}
 }
